a27d5d21a713c51e57298e7908c1f74864b7d096
Pinnochioooo <ashlinksunil@gmail.com>
2026-07-26 00:35:36 +0530

    Add: partition-equal-subset-sum - submission-1

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-1.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+
+    bool finder(vector<int>& nums,int target,int idx,vector<vector<int>>& dp){
+        if(target == 0)return true;
+        if(idx == nums.size())return false;
+
+        if(dp[idx][target]!= -1){
+            return dp[idx][target];
+        }
+
+        bool take = false;
+        if(nums[idx] <=  target){take = finder(nums,target-nums[idx],idx+1,dp);}
+        if(take)return dp[idx][target]=take;
+
+        bool nottaken = finder(nums,target,idx+1,dp);
+
+        return dp[idx][target]=take||nottaken;
+    }
+
+
+    bool canPartition(vector<int>& nums) {
+        int sum =0;
+        int idx =0;
+        
+
+
+        for(auto x: nums){
+            sum+=x;
+        }
+        if(sum%2)return false;
+       int target = sum/2;
+    vector<vector<int>> dp(nums.size(),vector<int>(target+1,-1));
+        return finder(nums,sum/2,0,dp);
+    }
+};
